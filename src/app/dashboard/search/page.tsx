9148ce2ebb5e68b54f2324f8c6c703c25c140a88
fibbbonachos <arish2k3@gmail.com>
2023-09-14 20:27:19 +0530

    update: add SWR api for clientside tune information requests wip

--- src/app/dashboard/search/page.tsx
+++ src/app/dashboard/search/page.tsx
@@ -1,8 +1,22 @@
+"use client";
+
 import React from "react";
 import useSWR from "swr";
 
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const query = "grenade";
+const apiKey = process.env.NEXT_PUBLIC_API_KEY || "ChickenWhopper";
+
 const page = () => {
-    return <div>search</div>;
+    const { data, error, isLoading } = useSWR(
+        `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&key=${apiKey}&regionCode=IN&videoCategoryId=10&maxResults=5&videoEmbeddable=true&order=relevance`,
+        fetcher
+    );
+
+    if (error) return <div>Failed to load</div>;
+    if (isLoading) return <div>Loading ...</div>;
+
+    return <div>{data.items[0].snippet.title}</div>;
 };
 
 export default page;
