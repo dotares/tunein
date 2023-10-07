0b53ee2c63ff6572ec4660cca5769226f287c459
fibbbonachos <arish2k3@gmail.com>
2023-10-07 14:05:25 +0530

    update: play and pause buttons work

--- src/app/_components/_playComponents/Play.tsx
+++ src/app/_components/_playComponents/Play.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState, useRef } from "react";
 import ReactPlayer from "react-player";
 
 interface PlayProps {
@@ -8,9 +8,30 @@ interface PlayProps {
 }
 
 const Play: React.FC<PlayProps> = ({ url }) => {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const ref = useRef(null);
+
     return (
         <div>
-            <ReactPlayer url={`https://www.youtube.com/watch?v=${url}`} />
+            <ReactPlayer
+                playing={isPlaying}
+                ref={ref}
+                url={`https://www.youtube.com/watch?v=${url}`}
+            />
+            <button
+                onClick={() => {
+                    setIsPlaying(true);
+                }}
+            >
+                Play
+            </button>
+            <button
+                onClick={() => {
+                    setIsPlaying(false);
+                }}
+            >
+                Pause
+            </button>
         </div>
     );
 };
