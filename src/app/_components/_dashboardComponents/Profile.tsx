3d20fa89858cd64153d937fed88d79c0646f0e7f
fibbbonachos <arish2k3@gmail.com>
2023-10-07 14:42:28 +0530

    remove: all components to rewrite the entire app

--- src/app/_components/_dashboardComponents/Profile.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React from "react";
-
-const Profile = () => {
-    return <div>Profile</div>;
-};
-
-export default Profile;
--- src/app/_components/_dashboardComponents/RecentlyPlayed.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React from "react";
-
-const RecentlyPlayed = () => {
-    return <div>RecentlyPlayed</div>;
-};
-
-export default RecentlyPlayed;
